2eecd3f9b417e215ab1840c1a18c4cfbd8ec3a26
MAD71241 <michele.deliso@gmail.com>
2021-07-06 16:27:41 +0200

    Ok query al db e visualizzazione a schermo

--- mysqli.php
+++ mysqli.php
@@ -2,7 +2,7 @@
 
 define("DB_HOST", "localhost");
 define("DB_USERNAME", "root");
-define("DB_PASSWORD", "");
+define("DB_PASSWORD", "root");
 define("DB_NAME", "university");
 
 $connection = new mysqli(DB_HOST, DB_USERNAME, DB_PASSWORD, DB_NAME);
@@ -13,4 +13,27 @@ if ($connection && $connection->connect_error) {
     die();
 }
 echo "Connection succesfull";
+
+$howManyCourses = "
+SELECT COUNT(id) AS numero_corsi,  department_id 
+FROM degrees 
+GROUP BY department_id";
+
+$result = $connection->query($howManyCourses);
+if ($result && $result->num_rows > 0) {
+    while ($a = $result->fetch_array()) {
+        ?>
+
+        <h2>ID dipartimento: <?php echo $a['department_id']; ?></h2>
+        <h2>Numero corsi: <?php echo $a['numero_corsi']; ?></h2> <?php
+        
+    }
+} elseif ($result) {
+    echo 'Nessun Risultato';
+  } else {
+    echo 'Errore nella query';
+  }
+
+$connection->close;
+
 ?>
